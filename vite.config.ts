7896c7496e122b2b072cd17e0bd332674308d105
Nathaniel Daley <ndaley64@gmail.com>
2024-12-01 21:17:25 -0600

    change local audio files to remote

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/markdown-previewer/",
+  base: "/drum-machine/",
   plugins: [react()],
   server: {
     port: 3000,
