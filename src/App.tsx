0d954eb72b5c7f5972f8f0ef218f13b9b4bcf89f
Nathaniel Daley <ndaley64@gmail.com>
2024-12-01 19:16:48 -0600

    import drum audio

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { AudioClip } from './types'
+
+const audioClips: AudioClip[] = [
+  {
+    keyTrigger: "Q",
+    url: "./assets/Heater-1.mp3",
+    description: "Heater 1",
+  },
+  {
+    keyTrigger: "W",
+    url: "./assets/Heater-2.mp3",
+    description: "Heater 2",
+  },
+  {
+    keyTrigger: "E",
+    url: "./assets/Heater-3.mp3",
+    description: "Heater 3",
+  },
+  {
+    keyTrigger: "A",
+    url: "./assets/Heater-4.mp3",
+    description: "Heater 4",
+  },
+  {
+    keyTrigger: "S",
+    url: "./assets/Clap.mp3",
+    description: "Clap",
+  },
+  {
+    keyTrigger: "D",
+    url: "./assets/Open-Hi-Hat.mp3",
+    description: "Open Hi-hat",
+  },
+  {
+    keyTrigger: "Z",
+    url: "./assets/Kick-n-Hat.mp3",
+    description: "Kick n' Hat",
+  },
+  {
+    keyTrigger: "X",
+    url: "./assets/Kick.mp3",
+    description: "Kick",
+  },
+  {
+    keyTrigger: "C",
+    url: "./assets/Closed-Hi-Hat.mp3",
+    description: "Closed Hi-hat",
+  },
+]
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      hello
     </>
   )
 }
